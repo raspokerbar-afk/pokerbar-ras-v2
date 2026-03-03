3f74b9e3ee54e7bd4615402a5ee9c5a9a4ce919a
田上太一 <tagamitaichi@tauetaichi-no-MacBook-Pro.local>
2026-03-03 16:21:10 +0900

    dotenv本番環境修正

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // PokerBar RAS API v1.4
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
